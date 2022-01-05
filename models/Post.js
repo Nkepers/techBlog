9936097e9714c7edc560c628d8ea8f959dbfe500
Nkepers <nick.kepers.coder@gmail.com>
2022-01-05 11:42:30 -0700

    updated routes

---
+++ models/Post.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our Post model
+class Post extends Model {};
+
+// create fields/columns for Post model
+Post.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    post_text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+module.exports = Post;
